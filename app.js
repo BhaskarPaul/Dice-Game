68b9ba208d20eccd6f1a6940b974bf0a446172cc
BhaskarPaul <53259501+BhaskarPaul@users.noreply.github.com>
2020-02-04 23:41:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
-randomNumberOne = Math.floor(Math.random()*6)+1;
-randomNumberTwo = Math.floor(Math.random() * 6) + 1;
-
-
-imageOne = document.querySelectorAll(".dice img")[0];
-imageTwo = document.querySelectorAll(".dice img")[1];
-heading = document.querySelector(".container h1")
-
-// Number one dice
-
-if (randomNumberOne === 1){
-    imageOne.setAttribute("src","./images/dice1.png");
-} else if (randomNumberOne === 2){
-    imageOne.setAttribute("src", "./images/dice2.png");
-} else if (randomNumberOne === 3){
-    imageOne.setAttribute("src", "./images/dice3.png");
-} else if (randomNumberOne === 4){
-    imageOne.setAttribute("src", "./images/dice4.png");
-} else if (randomNumberOne === 5){
-    imageOne.setAttribute("src", "./images/dice5.png");
-} else{
-    imageOne.setAttribute("src", "./images/dice6.png");
-}
-
-// Number two dice
-
-if (randomNumberTwo === 1) {
-    imageTwo.setAttribute("src", "./images/dice1.png");
-} else if (randomNumberTwo === 2) {
-    imageTwo.setAttribute("src", "./images/dice2.png");
-} else if (randomNumberTwo === 3) {
-    imageTwo.setAttribute("src", "./images/dice3.png");
-} else if (randomNumberTwo === 4) {
-    imageTwo.setAttribute("src", "./images/dice4.png");
-} else if (randomNumberTwo === 5) {
-    imageTwo.setAttribute("src", "./images/dice5.png");
-} else {
-    imageTwo.setAttribute("src", "./images/dice6.png");
-}
-
-if (randomNumberOne === randomNumberTwo){
-    heading.innerHTML = "Draw" ;
-} else if (randomNumberTwo > randomNumberOne){
-    heading.innerHTML = "Player 2 Wins 🚩";
-} else{
-    heading.innerHTML = "Player 1 Wins 🚩";
+var defaultValue = sessionStorage.getItem(".k") || 'defaultValue'; if (defaultValue == 'defaultValue') { document.querySelector(".container h1").textContent = "Refresh Me"; sessionStorage.setItem(".k", "1") } else {
+    randomNumberOne = Math.floor(Math.random() * 6) + 1; randomNumberTwo = Math.floor(Math.random() * 6) + 1; imageOne = document.querySelectorAll(".dice img")[0]; imageTwo = document.querySelectorAll(".dice img")[1]; heading = document.querySelector(".container h1")
+    if (randomNumberOne === 1) { imageOne.setAttribute("src", "./images/dice1.png") } else if (randomNumberOne === 2) { imageOne.setAttribute("src", "./images/dice2.png") } else if (randomNumberOne === 3) { imageOne.setAttribute("src", "./images/dice3.png") } else if (randomNumberOne === 4) { imageOne.setAttribute("src", "./images/dice4.png") } else if (randomNumberOne === 5) { imageOne.setAttribute("src", "./images/dice5.png") } else { imageOne.setAttribute("src", "./images/dice6.png") }
+    if (randomNumberTwo === 1) { imageTwo.setAttribute("src", "./images/dice1.png") } else if (randomNumberTwo === 2) { imageTwo.setAttribute("src", "./images/dice2.png") } else if (randomNumberTwo === 3) { imageTwo.setAttribute("src", "./images/dice3.png") } else if (randomNumberTwo === 4) { imageTwo.setAttribute("src", "./images/dice4.png") } else if (randomNumberTwo === 5) { imageTwo.setAttribute("src", "./images/dice5.png") } else { imageTwo.setAttribute("src", "./images/dice6.png") }
+    if (randomNumberOne === randomNumberTwo) { heading.innerHTML = "Draw" } else if (randomNumberTwo > randomNumberOne) { heading.innerHTML = "Player 2 Wins 🚩" } else { heading.innerHTML = "Player 1 Wins 🚩" }
 }
